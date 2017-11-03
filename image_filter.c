82bfe4573a0def9a015bc2bf3556927f568024ae
oguyon <oliv.guyon@gmail.com>
2017-11-03 09:06:18 -1000

    msync

--- image_filter.c
+++ image_filter.c
@@ -5,7 +5,7 @@
 #include <fitsio.h>  /* required by every program that uses CFITSIO  */
 
 
-#include "CLIcore.h"
+#include "CommandLineInterface/CLIcore.h"
 #include "00CORE/00CORE.h"
 #include "COREMOD_tools/COREMOD_tools.h"
 #include "COREMOD_memory/COREMOD_memory.h"
